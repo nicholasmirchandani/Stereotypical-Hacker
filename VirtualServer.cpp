1e8f0a464b22ba45441a7915eddc28e35722d374
nicholasmirchandani <nmirchandani@chapman.edu>
2020-05-05 00:08:32 -0700

    OMG I'm dumb

--- VirtualServer.cpp
+++ VirtualServer.cpp
@@ -4,8 +4,8 @@
 VirtualServer::VirtualServer() {
     //Default Constructor
     //Only setting these 2 attributes because all the others vary from server to server and are setup as needed
-    bool captured = false;
-    Player* currentPlayer = nullptr;
+    captured = false;
+    currentPlayer = nullptr;
 }
 
 bool VirtualServer::captureServer(std::string username, std::string password) {
