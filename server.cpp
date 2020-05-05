dc7bb0e872c28e059e01db48b9a4c18d3e49b667
nicholasmirchandani <nmirchandani@chapman.edu>
2020-05-04 20:32:44 -0700

    Attempt at removing command from args

--- server.cpp
+++ server.cpp
@@ -92,7 +92,7 @@ void listenPlayer(int playerSocket) {
                 }
             }
 
-            if(j != temp.length()) {
+            if(j != temp.length() - 1 && command != temp) {
                 //Adding the last element, not delineated by space
                 arguments.insert(arguments.end(), temp.substr(j));
             }
