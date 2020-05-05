cc3322b5d703aa60e5a9b064c4d2cd8547a50f38
nicholasmirchandani <nmirchandani@chapman.edu>
2020-05-04 21:46:27 -0700

    Adds debug print

--- VirtualServer.cpp
+++ VirtualServer.cpp
@@ -1,4 +1,5 @@
 #include "VirtualServer.h"
+#include <iostream> //DEBUG ONLY
 
 VirtualServer::VirtualServer() {
     //Default Constructor
@@ -8,6 +9,7 @@ VirtualServer::VirtualServer() {
 }
 
 bool VirtualServer::captureServer(std::string username, std::string password) {
+    std::cout << "DEBUG: Username: " << username << " RootUser: " << rootUser << " Password: " << password << " RootPass: " << rootPass << std::endl;
     if(username == rootUser && password == rootPass) {
         captured = true;
         return true;
